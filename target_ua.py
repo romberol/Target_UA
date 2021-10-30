ef792b5358e962e52e95fb61c542ba3673e8dc62
romberol <bernikov.roman.43@gmail.com>
2021-10-30 23:45:48 +0300

    Final version. Added main() and normalized for pylint

--- target_ua.py
+++ target_ua.py
@@ -9,8 +9,8 @@ def generate_grid():
     e.g. ['з', 'у', 'п', 'ж', 'ф']
     """
     grid = []
-    ua_letters = [1072, 1073, 1074, 1075, 1076, 1077, 1078, 1079, 1080, 1081, 1082, 1083, 
-    1084, 1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094, 1095, 1096, 1097, 
+    ua_letters = [1072, 1073, 1074, 1075, 1076, 1077, 1078, 1079, 1080, 1081, 1082, 1083,
+    1084, 1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094, 1095, 1096, 1097,
     1100, 1102, 1103, 1108, 1110, 1111, 1169]
     while len(grid)!=5:
         letter = random.randint(1072, 1169)
@@ -18,19 +18,20 @@ def generate_grid():
             grid.append(chr(letter))
     return grid
 
-def get_words(f, letters):
+def get_words(file_name, letters):
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
-    with open(f, encoding = 'utf-8') as file:
-        dict = {"/n":"noun", "noun":"noun", "/v":"verb", "verb":"verb", "/adj":"adjective", "adj":"adjective", "adv":"adverb"}
+    with open(file_name, encoding = 'utf-8') as file:
+        correct_dict = {"/n":"noun", "noun":"noun", "/v":"verb", "verb":"verb",
+        "/adj":"adjective", "adj":"adjective", "adv":"adverb"}
         word_list = []
         for line in file:
-            for key in dict:
+            for key, value in correct_dict.items():
                 word = line.split()[0]
                 if key in line and len(word)<=5:
-                    if word[0] in letters:
-                        word_list.append((word, dict[key]))
+                    if word[0] in letters and word not in word_list:
+                        word_list.append((word, value))
                     break
         return word_list
 
@@ -47,14 +48,24 @@ def check_user_words(user_words, language_part, letters, dict_of_words):
             correct_words.append(word)
     missed_words= []
     for word in dict_of_words:
-        if word[0] not in user_words:
+        if word[0] not in user_words and word[1]==language_part:
             missed_words.append(word[0])
-    
     return correct_words, missed_words
-    
 
-# let = generate_grid()
-let = ['у', 'ґ', 'и', 'л', 'ь']
-user_words = ['урема', 'устяж', 'усус']
-dict_of_words = get_words("E:/UCU/OP/ЛР 6/Target_ua/base.lst", let)
-print(check_user_words(user_words, 'noun', let, dict_of_words))
+
+def main():
+    """
+    Main function
+    """
+    grid = generate_grid()
+    print(grid)
+    language_part = random.choice(['noun', 'verb', 'adjective', 'adverb'])
+    print("Your category:", language_part)
+    user_words = list(input('Enter words: ').split())
+    dict_words = get_words("E:/UCU/OP/ЛР 6/Target_ua/base.lst", grid)
+    correct_words, missed_words = check_user_words(user_words, language_part, grid, dict_words)
+    print("Your correct words:", correct_words)
+    print("Your missed words:", missed_words)
+
+if __name__=="__main__":
+    main()
